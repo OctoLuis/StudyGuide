5ed5cdb05cbe760fd70ef258df9526ef049eb126
OctoLuis <73768287+OctoLuis@users.noreply.github.com>
2022-06-04 19:41:19 -0600

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -3,5 +3,6 @@ localStorage.setItem('unit_id', 'unit1'); // default value
 function getUnitID(button_id) {
     // unit_number = button_id;
     localStorage.setItem('unit_id', button_id);
+    localStorage.setItem('is_quiz', "false");
     // console.log(unit_number)
 }
